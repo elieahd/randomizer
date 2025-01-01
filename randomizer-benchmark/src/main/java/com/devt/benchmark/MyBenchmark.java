d5c542e4a5ebf9468e8f2093cbce7633934c6eee
Elie Daher <eliedhr@gmail.com>
2025-01-01 10:23:47 -0500

    not working

--- randomizer-benchmark/src/main/java/com/devt/benchmark/MyBenchmark.java
+++ randomizer-benchmark/src/main/java/com/devt/benchmark/MyBenchmark.java
@@ -1,11 +1,20 @@
 package com.devt.benchmark;
 
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
 import static com.devt.randomizer.RandomizerUtils.random;
 
+@State(Scope.Benchmark)
 public class MyBenchmark {
 
-    public static void main(String[] args) {
-        System.out.println(random(String.class));
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    public void randomString() {
+        random(String.class);
     }
 
 }

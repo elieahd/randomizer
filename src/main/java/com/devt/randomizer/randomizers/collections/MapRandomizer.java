4ba1861e21ca5a2ddaaa2d215e4a94650965c777
Elie Daher <eliedhr@gmail.com>
2025-01-01 10:52:40 -0500

    Update MapRandomizer.java

--- src/main/java/com/devt/randomizer/randomizers/collections/MapRandomizer.java
+++ src/main/java/com/devt/randomizer/randomizers/collections/MapRandomizer.java
@@ -25,6 +25,12 @@ public class MapRandomizer<K, V> implements Randomizer<Map<K, V>> {
 
     @Override
     public Map<K, V> next() {
+        // In the future, we should handle the case where key randomizer generates less than number of elements
+        // For example, in case we are generating a random map<Integer, String> of 120 elements
+        // with the key randomizer being an integer randomizer (0 -> 100) that means 101 key possibilities
+        // we will have infinite loop issue
+        // for now this situation won't happen, since the number of elements = 5 and is unmodifiable
+        // but in the future we might open the possibility to modify parameters, and we should fix this up before.
         Map<K, V> result = new HashMap<>();
         while (result.size() < numberOfElements) {
             K key = keyRandomizer.next();
